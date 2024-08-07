22ae0ab930860ddf39a1eff5e2d4b5516738a2ca
Prayag Joshi <prayagjoshi263@gmail.com>
2024-08-07 14:54:09 +0530

    feat: Established database connection with mongoDB atlas;

--- server-app/src/server.ts
+++ server-app/src/server.ts
@@ -1,9 +1,10 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
+import connectDb from "./config/dbConfig";
 
 dotenv.config();
-
+connectDb();
 const port = process.env.PORT || 5001;
 
 const app = express();
